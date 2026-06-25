1010c700969abb18c262dd59f0762000981af434
Dang Ngoc Thuy An <thuyyann765@gmail.com>
2026-06-25 09:45:09 +0700

    feat: them interface Fibonacii

---
+++ DangNgocThuyAn/module1/fibonacci.js
@@ -0,0 +1,10 @@
+/**
+ * Interface Fibonacci
+ * @param {number} n - so thu n trong day Fibonacci
+ * @param {boolean} flag - true: dung de quy, false: dung vong lap
+ */
+function Fibonacci(n, flag) {
+    // Implementation se duoc them o cac branch impl1 va impl2
+}
+
+module.exports = { Fibonacci };
