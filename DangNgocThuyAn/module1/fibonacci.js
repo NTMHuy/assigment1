ba8c40e96ad10a8e2aeb249aa6f2ce8e80c275d4
Dang Ngoc Thuy An <thuyyann765@gmail.com>
2026-06-25 10:00:28 +0700

    feat: implement Fibonacci vong lap (flag = false)

---
+++ DangNgocThuyAn/module1/fibonacci.js
@@ -0,0 +1,20 @@
+/**
+ * Tinh so Fibonacci
+ * @param {number} n - so thu n
+ * @param {boolean} flag - true: de quy, false: vong lap
+ */
+function Fibonacci(n, flag) {
+    if (flag === false) {
+        // Vong lap
+        if (n <= 1) return n;
+        let a = 0, b = 1;
+        for (let i = 2; i <= n; i++) {
+            let temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
+
+module.exports = { Fibonacci };
